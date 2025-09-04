d6c3113aa8cf9f233e95529577431b86061a7e76
devbattery <devbattery@outlook.com>
2025-09-04 20:41:59 +0900

    Feat[Move Fast]: Pydantic Configurations

--- books2.py
+++ books2.py
@@ -22,11 +22,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description='ID is not needed on create', default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)  # range(1 to 5)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "codingWithRoby",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
 
 
 BOOKS = [

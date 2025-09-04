5fc47b67ee499a93a7c9add15fe53449a1b64a41
devbattery <devbattery@outlook.com>
2025-09-04 21:08:18 +0900

    Feat[Move Fast]: Assignment Solution

--- books2.py
+++ books2.py
@@ -12,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -27,6 +29,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)  # range(1 to 5)
+    published_date: int = Field(gt=1999, lt=2031)
 
     model_config = {
         "json_schema_extra": {
@@ -34,19 +37,20 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "codingWithRoby",
                 "description": "A new description of a book",
-                "rating": 5
+                "rating": 5,
+                'published_date': 2029
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'codingWithRoby', 'A very nice book!', 5),
-    Book(2, 'Be Fast with FastAPI', 'codingWithRoby', 'A great book!', 5),
-    Book(3, 'Master Endpoints', 'codingWithRoby', 'A awesome book!', 5),
-    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
-    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
-    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+    Book(1, 'Computer Science Pro', 'codingWithRoby', 'A very nice book!', 5, 2030),
+    Book(2, 'Be Fast with FastAPI', 'codingWithRoby', 'A great book!', 5, 2030),
+    Book(3, 'Master Endpoints', 'codingWithRoby', 'A awesome book!', 5, 2029),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2028),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2027),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
 ]
 
 
@@ -69,6 +73,16 @@ async def read_book_by_rating(book_rating: int):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/api/books/publish/")
+async def read_books_by_publish_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
             books_to_return.append(book)
 
     return books_to_return

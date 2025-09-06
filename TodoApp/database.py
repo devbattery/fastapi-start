2952dceac40899f92fa329a228160952ad818edd
devbattery <devbattery@outlook.com>
2025-09-06 15:14:49 +0900

    Feat[API Request Methods]: Get All Todos from Database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -6,6 +6,6 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-SessionLocal = sessionmaker(autoflush=False)
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 Base = declarative_base()
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 from TodoApp import models
-from TodoApp.database import engine
+from TodoApp.database import engine, SessionLocal
+from TodoApp.models import Todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()

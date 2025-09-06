fb0dbed74125ececa200aac2a0df97ac7d060396
devbattery <devbattery@outlook.com>
2025-09-06 16:42:24 +0900

    Feat[Authentication & Authorization]: Create First User

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,31 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from starlette import status
+
+from TodoApp.models import Users
 
 router = APIRouter()
 
 
-@router.get("/api/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+class CreateUserRequest(BaseModel):
+    nickname: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/api/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        nickname=create_user_request.nickname,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
+
+    return create_user_model

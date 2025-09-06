47a23d7c9b15f71d57bda9c285264d41b3298995
devbattery <devbattery@outlook.com>
2025-09-06 22:17:27 +0900

    Feat[Authenticate Requests]: Admin Router

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from TodoApp import models
 from TodoApp.database import engine
-from TodoApp.routers import auth, todos
+from TodoApp.routers import auth, todos, admin
 
 app = FastAPI()
 
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)

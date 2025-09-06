5996ee26ee1898d893185b642868f235c0653ea9
devbattery <devbattery@outlook.com>
2025-09-06 15:28:06 +0900

    Feat[API Request Methods]: Post Request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -24,6 +25,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/api/todos")
 async def read_todos_all(db: db_dependency):
     return db.query(Todos).all()
@@ -36,3 +44,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
         return todo_model
 
     raise HTTPException(status_code=404, detail='Todo not found.')
+
+@app.post("/api/todos", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()

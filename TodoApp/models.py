d2a89f6a40aab2c3e65ea62724d4df162184d5de
devbattery <devbattery@outlook.com>
2025-09-06 16:34:42 +0900

    Feat[Authentication & Authorization]: Users Table Creation

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,6 +1,19 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 from TodoApp.database import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    nickname = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todos(Base):
@@ -11,3 +24,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))

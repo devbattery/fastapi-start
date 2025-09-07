bd7fcc7a701bb74d55f414b5c64466d2c4700c26
devbattery <devbattery@outlook.com>
2025-09-07 21:35:20 +0900

    Feat[Unit & Integration Testing]: FastAPI Project Test Part 10

--- TodoApp/test/test_auth.py
+++ TodoApp/test/test_auth.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+
+from jose import jwt
+
 from TodoApp.main import app
-from TodoApp.routers.auth import get_db, authenticate_user
+from TodoApp.routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
 from TodoApp.test.utils import override_get_db, TestingSessionLocal, test_user
 
 app.dependency_overrides[get_db] = override_get_db
@@ -17,3 +21,18 @@ def test_authenticate_user(test_user):
 
     wrong_password_user = authenticate_user(test_user.username, 'WrongPassword', db)
     assert wrong_password_user is False
+
+
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days=1)
+
+    token = create_access_token(username, user_id, role, expires_delta)
+
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], options={'verify_signature': False})
+
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role

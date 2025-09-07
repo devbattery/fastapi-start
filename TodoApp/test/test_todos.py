851bbb801fa2879fab89fe5383a76d7c5bb4b58a
devbattery <devbattery@outlook.com>
2025-09-07 20:53:08 +0900

    Feat[Unit & Integration Testing]: FastAPI Project Test Part 4

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,64 +1,11 @@
-import pytest
-from sqlalchemy import create_engine, StaticPool, text
-from sqlalchemy.orm import sessionmaker
 from starlette import status
-from starlette.testclient import TestClient
 
-from TodoApp.database import Base
-from TodoApp.main import app
-from TodoApp.models import Todos
 from TodoApp.routers.auth import get_current_user
 from TodoApp.routers.todos import get_db
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool
-)
-
-TestingSessionLocal = sessionmaker(autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-def override_get_current_user():
-    return {'username': 'redhood', 'id': 1, 'user_role': 'admin'}
-
+from TodoApp.test.utils import *
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
-
-client = TestClient(app)
-
-
-@pytest.fixture
-def test_todo():
-    todo = Todos(
-        title='Learn to code!',
-        description='Need to learn everyday!',
-        priority=5,
-        complete=False,
-        owner_id=1
-    )
-
-    db = TestingSessionLocal()
-    db.add(todo)
-    db.commit()
-
-    yield todo
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM todos;"))
-        connection.commit()
 
 
 def test_read_all_authenticated(test_todo):
